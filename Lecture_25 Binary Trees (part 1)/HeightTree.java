1c557e790887955eeb57548f2a2e5ce9fa14a9fb
Jaymin <jaymindarji2003@gmail.com>
2023-03-23 06:45:39 +0530

    Finished Binary Tree part 1

---
+++ Lecture_25 Binary Trees (part 1)/HeightTree.java
@@ -0,0 +1,56 @@
+/*
+    Calculte height of a tree
+*/
+
+public class HeightTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    //height fnx
+    public static int height(Node root){
+        // base case
+        if(root == null){
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+        int height = Math.max(lh, rh) + 1;
+        
+        return height;
+    }
+
+    public static void main(String[] args) {
+        /*
+            Tree:-
+            
+                     1
+                   /   \
+                  2     3
+                 / \   / \
+                4   5 6   7 
+        */
+
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(height(root));
+    }
+}
---
+++ Lecture_25 Binary Trees (part 1)/mix.java
@@ -0,0 +1,75 @@
+
+import java.util.*;
+
+public class mix {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        // build tree fnx
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        // level order traversal
+        public void LevelorderTraversal(Node root) {
+            // base case
+            if (root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(currNode);
+                    }
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if (currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+
+        tree.LevelorderTraversal(root);
+    }   
+}
