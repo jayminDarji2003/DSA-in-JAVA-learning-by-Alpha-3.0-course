a7c6896c66e9bc5a1888dbf3808e45b4e93c336e
Jaymin <jaymindarji2003@gmail.com>
2023-06-02 09:39:08 +0530

    Start solving problems on Trie

---
+++ Lecture_32 Tries/MIX.java
@@ -0,0 +1,39 @@
+/*
+    Insertion in a Trie.
+        --> Insert a new string in a Trie.
+*/
+
+public class MIX {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean efw = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.efw = true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "any", "thee" };
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+    }
+}
--- Lecture_32 Tries/SearchTrie.java
+++ Lecture_32 Tries/SearchTrie.java
@@ -1,14 +1,14 @@
 /*
-    Insertion in Trie :-
-        Adding a one by one into a Trie.
+    Search in a Trie.
+        -> To search a word in a Trie.
 */
 
-public class Insertion {
+public class SearchTrie {
     static class Node{
         Node children[] = new Node[26];
         boolean efw = false;
 
-        public Node(){
+        Node(){
             for(int i=0; i<26; i++){
                 children[i] = null;
             }
@@ -17,7 +17,7 @@ public class Insertion {
 
     static Node root = new Node();
 
-    // insert fnx
+    //insert fnx
     public static void insert(String word){
         Node curr = root;
 
@@ -35,10 +35,31 @@ public class Insertion {
         curr.efw = true;
     }
 
+
+    // Search fnx
+    public static boolean search(String word){  // TC O(l)
+        Node curr = root;
+
+        for(int level = 0; level < word.length(); level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.efw == true;
+    }
+
     public static void main(String[] args) {
         String words[] = {"the" , "a" ,"there" , "their" , "any" , "thee"};
+        
+        // insert a word
         for(int i=0; i<words.length; i++){
             insert(words[i]);
         }
+
+        System.out.println(search("thee"));
+        System.out.println(search("thor"));
     }
 }
