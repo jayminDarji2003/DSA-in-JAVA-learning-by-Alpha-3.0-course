db88920479aa82dbac6c76f475043880195e0179
Jaymin <jaymindarji2003@gmail.com>
2023-06-30 10:40:22 +0530

    Done with the new question

---
+++ Lecture_32 Tries/Q_4.java
@@ -0,0 +1,58 @@
+// Q.4 :- Count Unique SubStrings
+
+public class Q_4 {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        public Node() {
+            for (int i = 0; i < children.length; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    // insert fnx
+    public static void insert(String word) {
+        Node curr = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int count = 0;
+        for(int i=0; i<26; i++){
+            if(root.children[i] != null){
+                count += countNodes(root.children[i]);
+            }
+        }
+
+        return count+1;
+    }
+
+    public static void main(String[] args) {
+        String str = "ababa"; // ans = 10
+
+        // step 1 :- cal suffix and then insert in Trie
+        for (int i = 0; i < str.length(); i++) {
+            String suf = str.substring(i);
+            insert(suf);
+        }
+
+        System.out.println(countNodes(root));
+    }
+}
