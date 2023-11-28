10c73a323e1ea67d2c22d570664eb51b9743bacc
Jaymin Darji <jaymindarji2003@gmail.com>
2023-11-28 17:35:06 +0530

    Day 11 starting

---
+++ Lecture_34 Graphs (part 2)/Revision.java
@@ -0,0 +1,33 @@
+/*
+    In this program i will revise four topics of Graph.
+        1. create a graph
+        2. bfs
+        3. dfs
+        4. has path?
+*/
+
+public class Revision {
+    // Edge class
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    // create a graph function
+    public static void createGraph(){
+
+    } 
+
+    public static void main(String[] args) {
+        int V = 7;
+        
+
+    }
+}
