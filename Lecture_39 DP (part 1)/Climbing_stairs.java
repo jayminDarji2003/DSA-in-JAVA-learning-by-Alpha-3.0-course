d6f7e5a0766c927b89974a7250f64b315c080591
Jaymin Darji <jaymindarji2003@gmail.com>
2023-12-12 00:15:12 +0530

    Day 24 done

--- Lecture_39 DP (part 1)/Climbing_stairs.java
+++ Lecture_39 DP (part 1)/Climbing_stairs.java
@@ -16,38 +16,65 @@
             combine both ways --> n ways
 */
 
+import java.util.Arrays;
+
 public class Climbing_stairs {
 
     // Recursive approach
-    public static int countStair_recursive(int n){
+    // O(2^n)
+    public static int countStair_recursive(int n) {
         // base case
-        if(n == 0){
+        if (n == 0) {
             return 1;
         }
-        if(n < 0){
+        if (n < 0) {
             return 0;
         }
 
-        return countStair_recursive(n-1) + countStair_recursive(n-2);
+        return countStair_recursive(n - 1) + countStair_recursive(n - 2);
     }
 
     // Memoization approach
-    public static int countStair_memoization(int n, int[] dp){
+    // O(n)
+    public static int countStair_memoization(int n, int[] dp) {
         // base case
-        if(n == 0) return 1;
-        if(n < 0) return 0;
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 0;
 
-        if(dp[n] != 0){
+        if (dp[n] != 0) {
             return dp[n];
         }
 
-        dp[n] = countStair_memoization(n-1, dp) + countStair_memoization(n-2, dp);
+        dp[n] = countStair_memoization(n - 1, dp) + countStair_memoization(n - 2, dp);
         return dp[n];
     }
-    
+
+    // Tabulation approach
+    // O(n)
+    public static int countStair_Tabulation(int n) {
+        int dp[] = new int[n + 1];
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1] + 0;
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
-        int n = 5;  
-        System.out.println("Recursive approach ways = " + countStair_recursive(n));      
-        System.out.println("Recursive approach ways = " + countStair_memoization(n, new int[n+1]));      
+        int n = 5;
+        System.out.println("Recursive approach ways = " + countStair_recursive(n));
+
+        System.out.println("Memoization approach ways = " + countStair_memoization(n, new int[n + 1]));
+
+        System.out.println("Tabulation approach ways = " + countStair_Tabulation(n));
     }
 }
