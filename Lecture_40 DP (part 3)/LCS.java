df2f73b135a441a6531149b8b54a72721ae49136
Jaymin Darji <jaymindarji2003@gmail.com>
2023-12-18 22:05:04 +0530

    Day 31 done

--- Lecture_40 DP (part 3)/LCS.java
+++ Lecture_40 DP (part 3)/LCS.java
@@ -41,6 +41,35 @@ public class LCS {
         }
     }
 
+    // Tabulation way
+    public static int lcsTabu(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
     public static void main(String[] args) {
         String str1 = "abcdge";
         String str2 = "abege";
@@ -54,6 +83,9 @@ public class LCS {
                 dp[i][j] = -1;
             }
         }
+
         System.out.println("Memoization way ans = " + lcsMemo(str1, str2, n, m, dp));
+
+        System.out.println("Tabulation way ans = " + lcsTabu(str1, str2));
     }
 }
